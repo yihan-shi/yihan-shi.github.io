fbf21dfa2e60592d22736ab338f16956b6194d2e
Yihan Shi <yihan.shiwork@gmail.com>
2025-07-18 02:49:01 -0400

    Add experience

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -27,11 +27,32 @@ export default function Experience() {
                 <li className="text-[#737373]">
                   <span className="mr-1">Aug 2024 – Present</span>
                   <span className="mx-1 max-lg:hidden">|</span>
-                  <span className="mx-1 max-lg:hidden">Washington, D.C.</span>
+                  <span className="mx-1 max-lg:hidden">Washington, DC</span>
                 </li>
               </ol>
             </div>
             <div className="my-1">
+              <ol className="list-outside list-disc space-y-1 px-3">
+                <li className="px-1">
+                  Solved a company-wide data bottleneck by independently developing 
+                  a pipeline to automate downloading, streaming, and structuring messy
+                  monthly JSON files, overcoming computing power limitations
+                </li>
+                <li className="px-1">
+                  Co-authored and fact-checked research papers and comment letters with
+                  economists and lawyers on the Inflation Reduction Act, pharmaceutical
+                  import security investigations, and health insurance price transparency
+                </li>
+                <li className="px-1">
+                  Managed the full intern hiring process from application review to final
+                  decision and provided mentorship and supervision to the selected intern
+                </li>
+                <li className="px-1">
+                  Improved data access by web scraping U.S. prescription drug policy
+                  information from government websites, SEC reports, and court rulings
+                  using Python
+                </li>
+              </ol>
             </div>
           </div>
         </li>
@@ -63,19 +84,20 @@ export default function Experience() {
             <div className="my-1">
               <ol className="list-outside list-disc space-y-1 px-3">
                 <li className="px-1">
-                  Developed full-stack web application features using the .NET Framework
-                  for enterprise customers such as McDonald’s, Walmart, and Bank of America,
-                  enabling large-scale document permission management, lookup, editing,
-                  uploading, and deletion services
-                </li>
-                <li className="px-1">
-                  Revitalized user experience by refactoring CSS, HTML, JavaScript
-                  code to align with industry-leading accessibility and UX standards
-                </li>
-                <li className="px-1">
                   Led an innovative research project using variational autoencoders
                   (a type of artificial neural network) to predict counterfactual
-                  campaign outcomes as part of a company AI initiative
+                  campaign outcomes in a company-wide AI initiative
+                </li>
+                <li className="px-1">
+                  Collaborated with UX designers and product managers to revamp the
+                  customer experience, refactoring CSS, HTML, and JavaScript to meet
+                  industry-leading accessibility and UX standards
+                </li>
+                <li className="px-1">
+                  Developed full-stack web application features to serve enterprise
+                  customers such as McDonald’s, Walmart, and BofA, allowing large-scale
+                  document permission management, lookup, update, upload, and deletion
+                  services
                 </li>
               </ol>
             </div>
@@ -104,34 +126,27 @@ export default function Experience() {
                 <li className="text-[#737373]">
                   <span className="mr-1">May 2022 – Aug 2022</span>
                   <span className="mx-1 max-lg:hidden">|</span>
-                  <span className="mx-1 max-lg:hidden">San Mateo, CA</span>
+                  <span className="mx-1 max-lg:hidden">San Francisco, CA</span>
                 </li>
               </ol>
             </div>
             <div className="my-1">
               <ol className="list-outside list-disc space-y-1 px-3">
                 <li className="px-1">
-                  Developed a data visualization dashboard with 10 progress-tracking
-                  metrics and presented at a client-facing Product Demo session
+                  Built a client-facing dashboard that tracks 10 key performance
+                  metrics and presented it in a live product demonstration
                 </li>
                 <li className="px-1">
-                  Communicated core needs with the engineering team to speed up their
-                  data querying process by designing SQLAlchemy wrappers to scalably query OLAP
+                  Created a reusable template that enables engineers to auto-generate
+                  webpages for the pre-training process in a machine learning workflow
                 </li>
                 <li className="px-1">
-                  Designed a template and workflow document for 2 NLP scientists to
-                  auto-generate HTML webpages for machine learning pre-training
+                  Implemented an automated data-cleaning pipeline that reduced
+                  bioinformaticians’ housekeeping time by 30%
                 </li>
                 <li className="px-1">
-                  Reduced 30% of the time bioinformaticians spent on housekeeping tasks by
-                  implementing a comprehensive and robust data cleaning process to reduce
-                  platform test errors systematically
-                </li>
-                <li className="px-1">
-                  Saved 50% of the manual labor of patient profile simulation by building
-                  and maintaining an infrastructure that streamlined and scaled 100x simulation
-                  based on real-world distribution multi-dimensional array of data in
-                  PostgreSQL databases
+                  Developed infrastructure that runs simulations of patient profiles
+                  100x faster, reducing manual effort by 50%
                 </li>
               </ol>
             </div>
@@ -158,7 +173,7 @@ export default function Experience() {
                   Duke University, School of Medicine
                 </li>
                 <li className="text-[#737373]">
-                  <span className="mr-1">Jun 2021 – Aug 2021</span>
+                  <span className="mr-1">May 2021 – Aug 2021</span>
                   <span className="mx-1 max-lg:hidden">|</span>
                   <span className="mx-1 max-lg:hidden">Durham, NC</span>
                 </li>
@@ -168,19 +183,15 @@ export default function Experience() {
               <ol className="list-outside list-disc space-y-1 px-3">
                 <li className="px-1">
                   Led a team of 3 undergraduate and postdoc students to craft a
-                  rare disease genomic data science project end-to-end
+                  rare disease genomic data science project
                 </li>
                 <li className="px-1">
-                  Identified 20 significant genes out of 35,000 using
-                  generalized linear model and effect size shrinkage
+                  Identified 20+ significant genes out of 35,000 using statistical
+                  techniques such as effect size shrinkage and blind dispersion estimation
                 </li>
                 <li className="px-1">
-                  Publishing a manuscript (in review) and designing future
-                  research projects with clinical researchers and pediatricians
-                </li>
-                <li className="px-1">
-                  Mentored 2 incoming first-year researchers and drafted
-                  biomedical data science starter projects
+                  Mentored 2 incoming first-year researchers and drafted biomedical data
+                  science starter projects
                 </li>
               </ol>
             </div>
@@ -202,7 +213,7 @@ export default function Experience() {
                   Duke University
                 </li>
                 <li className="text-[#737373]">
-                  <span className="mr-1">Aug 2021 – Aug 2023</span>
+                  <span className="mr-1">Aug 2021 – May 2023</span>
                   <span className="mx-1 max-lg:hidden">|</span>
                   <span className="mx-1 max-lg:hidden">Durham, NC</span>
                 </li>
@@ -211,12 +222,14 @@ export default function Experience() {
             <div className="my-1">
               <ol className="list-outside list-disc space-y-1 px-3">
                 <li className="px-1">
-                  Authored technical tutorial for doctors on recurrent event
-                  survival analysis methods to examine large claim datasets
+                  Co-authored internal tutorial on recurrent event analysis to
+                  examine instances of alcohol-related complications after bariatric
+                  surgeries
                 </li>
                 <li className="px-1">
-                  Authored manuscript (in review) on the impact of confounding
-                  medical interventions in electronic health records data
+                  Investigated algorithmic bias of inappropriate proxy variables
+                  (e.g., race, gender, and ZIP Code) in Electronic Health Record data
+                  using simulation
                 </li>
               </ol>
             </div>
